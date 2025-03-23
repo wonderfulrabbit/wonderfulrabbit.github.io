1c8789f76aa203c51e024df546f75a6ac092c1b1
usagi <woshawott@gmail.com>
2025-03-23 11:04:39 -0500

    Auto update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 import { initializeNavigation } from "./navigation.js";
-import { initializeDropdown } from "./dropdown.js";
 import { loadData } from "./dataLoader.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     initializeNavigation();
-    initializeDropdown();
     loadData();
 });
 
